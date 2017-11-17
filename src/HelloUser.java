3d692122c3424aa5dc7df2a82956abf15cf05df6
thomas-schymro <thomas.schymroszczyk@tum.de>
2017-11-17 11:34:24 +0100

    Hello User hinzugefügt

---
+++ src/HelloUser.java
@@ -0,0 +1,12 @@
+
+public class HelloUser {
+
+	String username;
+	
+	public void greetuser () {
+		
+		System.out.println("Hello "+ username);
+	
+		
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,18 @@
+import java.io.*;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	public static void main(String[] args) throws IOException {
+		
+		HelloUser user = new HelloUser();
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.print("Geben Sie den Username ein: ");
+		user.username = br.readLine();
+		user.greetuser();
+		
+		// TODO Auto-generated method stu b
 		System.out.println("Hello ASE2017 - How are you now? :)");
 	}
 
